541fe2cf6d98573dff01d7b1b26bda4e111ff7ab
zych.johann@gmail.com <zych.johann@gmail.com>
2022-09-26 22:47:49 +0200

    Dev mobile first

---
+++ script.js
@@ -0,0 +1,40 @@
+function voirsuite1()
+{
+	document.getElementById('voirsuite1').style.display='none';   
+    document.getElementById('suite1').style.display='block';   
+}
+function voirsuite2()
+{
+	document.getElementById('voirsuite2').style.display='none';   
+    document.getElementById('suite2').style.display='block';   
+}
+function voirsuite3()
+{
+	document.getElementById('voirsuite3').style.display='none';   
+    document.getElementById('suite3').style.display='block';   
+}
+function voirsuite4()
+{
+	document.getElementById('voirsuite4').style.display='none';   
+    document.getElementById('suite4').style.display='block';   
+}
+function replier1()
+{
+    document.getElementById('voirsuite1').style.display='block';   
+    document.getElementById('suite1').style.display='none';
+}
+function replier2()
+{
+    document.getElementById('voirsuite2').style.display='block';   
+    document.getElementById('suite2').style.display='none';
+}
+function replier3()
+{
+    document.getElementById('voirsuite3').style.display='block';   
+    document.getElementById('suite3').style.display='none';
+}
+function replier4()
+{
+    document.getElementById('voirsuite4').style.display='block';   
+    document.getElementById('suite4').style.display='none';
+}
