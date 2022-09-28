479ba05010d039fc7ceae87ce02b336c6f3f6d5d
AnthonyMarche <marche.anthony72@gmail.com>
2022-09-28 15:07:27 +0200

    maj img

--- index.php
+++ index.php
@@ -52,8 +52,8 @@ require 'elements/data.php';
             <h1 id="fName">jon</h1>
             <h1 id="lName">SNOW</h1>
                 <div class="logoReseaux">
-                <a href="https://twitter.com/LordSnow" target="_blank"><img src="/PNG/twitter-logo.png" class ="twitter" alt="logo lien twitter"></a>
-                <a href="https://uk.linkedin.com/in/jon-snow-3b1245192/fr?trk=people-guest_people_search-card" target="_blank"><img src="/PNG/linkedin-logo.png" class="linkedin" alt="logo lien linkedin"></a>
+                <a href="https://twitter.com/LordSnow" target="_blank"><img src="/img/twitter-logo.png" class ="twitter" alt="logo lien twitter"></a>
+                <a href="https://uk.linkedin.com/in/jon-snow-3b1245192/fr?trk=people-guest_people_search-card" target="_blank"><img src="/img/linkedin-logo.png" class="linkedin" alt="logo lien linkedin"></a>
                 
                 
                 
