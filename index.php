5cd84de8e30e8e0911fe0f6185219d11b0bf1901
AnthonyMarche <marche.anthony72@gmail.com>
2022-10-03 15:21:23 +0200

    update html + animation mobile

--- index.php
+++ index.php
@@ -11,49 +11,35 @@ require_once 'elements/data.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cv Jon Snow</title>
 
-    <link rel="stylesheet" media='screen and (max-width: 930px)' href="styleMobile.css"/>
-    <link rel="stylesheet" media='screen and (min-width: 931px)' href="styleDesktop.css"/>
-    <link rel="stylesheet"  href="style.css"/>
-
+    <link rel="stylesheet" media='screen and (max-width: 930px)' href="styleMobile.css" />
+    <link rel="stylesheet" media='screen and (min-width: 931px)' href="styleDesktop.css" />
+    <link rel="stylesheet" href="style.css" />
 </head>
 
-<body class="body">
+<body>
 
     <main>
+        <script src="https://kit.fontawesome.com/67a2af0157.js" crossorigin="anonymous"></script>
 
-  <script src="https://kit.fontawesome.com/67a2af0157.js" crossorigin="anonymous">
-
-        </script>
         <div class="buttons">
             <div class="button">
                 <i id="button-on" class="fa-solid fa-toggle-on" onclick="darkmode()"></i>
-
-
                 <i id="button-off" class="fa-solid fa-toggle-off" onclick="lightmode()"></i>
-
-
-
-
             </div>
 
-
-
-
             <div id="lang-selection">
-                <form action="index.php" method ="get">
+                <form action="index.php" method="get">
                     <label for="language" class="font-language">Select a language</label>
-                    <select name="language" id="language" >
+                    <select name="language" id="language">
                         <option class="lang" value="FR">FR</option>
                         <option class="lang" value="EN">EN</option>
                         <option class="lang" value="VAL">VAL</option>
                         <input type="submit" id="submit-button" value="Change">
                     </select>
                 </form>
-
             </div>
-
-
         </div>
+
         <div class="logoReseaux">
             <a href="https://twitter.com/LordSnow" target="_blank"><img src="/img/twitter-icon-25px.png" class="twitter" alt="logo lien twitter"></a>
             <a href="https://uk.linkedin.com/in/jon-snow-3b1245192/fr?trk=people-guest_people_search-card" target="_blank"><img src="/img/linkedin-icon-25px.png" class="linkedin" alt="logo lien linkedin"></a>
@@ -61,30 +47,29 @@ require_once 'elements/data.php';
 
         <header>
             <img src="/img/JonSnow.jpg" alt="Profil" srcset="">
-            <h1 id="fName">jon</h1>
+            <h1 id="fName">Jon</h1>
             <h1 id="lName">SNOW</h1>
-            <div class="logoGoT">
-                <img src="/img/stark2.png" class ="stark" alt="logo stark">
-                <img src="/img/targaryen2.png" class="targaryen" alt="logo targaryen">
-                <img src="/img/stark1.png" class ="stark1" alt="logo stark">
-                <img src="/img/targaryen1.png" class="targaryen1" alt="logo targaryen">
+            <h1 id="name">Jon SNOW</h1>
 
-            </div>
         </header>
+
         <div id="about">
+            <div class="logoGoT">
+                <img src="/img/stark2.png" class="stark" alt="logo stark">
+                <img src="/img/targaryen2.png" class="targaryen" alt="logo targaryen">
+                <img src="/img/stark1.png" class="stark1" alt="logo stark">
+                <img src="/img/targaryen1.png" class="targaryen1" alt="logo targaryen">
+            </div>
             <h2>Team leader</h2>
             <p><?= $data[4][0] ?></p>
         </div>
+
         <section class="info">
-
-            <article>
-                <a href="#article1">
-                    <h2 class="more1">
-                        <?= $data[0][0] ?>
-                    </h2>
-                </a>
-
-                <div class="text1">
+            <article class="block">
+                <div class="title-article">
+                    <h2><?= $data[0][0] ?></h2>
+                </div>
+                <div class="text">
                     <?php foreach ($data[1] as $value) {
                         echo $value . "<br>";
                     }
@@ -92,57 +77,47 @@ require_once 'elements/data.php';
                     <br>
                 </div>
             </article>
-        <article>
-            <a href="#" onclick="voirsuite2();" id="voirsuite2">
 
+            <article class="block">
+                <div class="title-article">
+                    <h2><?= $data[0][1] ?></h2>
+                </div>
+                <div class="text">
+                    <?php foreach ($data[2] as $value) {
+                        echo $value . "<br>";
+                    }
+                    ?>
+                    <br>
+                </div>
+            </article>
 
-                <h2><?= $data[0][1] ?></h2>
-            </a>
-            <div id="suite2">
-                <h2><?= $data[0][1] ?></h2>
-                <?php foreach ($data[2] as $value) {
-                    echo $value . "<br>";
-                }
-                ?><br />
+            <article class="block">
+                <div class="title-article">
+                    <h2><?= $data[0][2] ?></h2>
+                </div>
+                <div class="text">
+                    <?php foreach ($data[3] as $value) {
+                        echo $value . "<br>";
+                    }
+                    ?>
+                    <br>
+                </div>
+            </article>
 
-                <a href="#" onclick="replier2();" id="replier2">X</a>
-            </div>
-        </article>
-        <article>
-            <a href="#" onclick="voirsuite3();" id="voirsuite3">
-
-                <h2><?= $data[0][2] ?></h2>
-            </a>
-            <div id="suite3">
-                <h2><?= $data[0][2] ?></h2>
-                <?php foreach ($data[3] as $value) {
-                    echo $value . "<br>";
-                }
-                ?><br />
-
-
-                <a href="#" onclick="replier3();" id="replier3">X</a>
-            </div>
-        </article>
-        <article>
-
-            <a href="#" onclick="voirsuite4();" id="voirsuite4">
-                <h2>Contact</h2>
-            </a>
-            <div id="suite4">
-                <h2>Contact</h2>
-                Mobile : 01.23.45.67.89<br />
-                <a href="mailto:jon.snow@gameofthrones">@ jon.snow@gameofthrones</a><br /><br />
-                <a href="#" onclick="replier4();" id="replier4">X</a>
-            </div>
-        </article>
-
+            <article class="block">
+                <div class="title-article">
+                    <h2>Contact</h2>
+                </div>
+                <div class="text">
+                    Mobile : 01.23.45.67.89<br />
+                    <a href="mailto:jon.snow@gameofthrones">@ jon.snow@gameofthrones</a><br /><br />
+                    <br>
+                </div>
+            </article>
         </section>
-
     </main>
 
     <script src="/script.js"></script>
-
 </body>
 
 </html>
--- script.js
+++ script.js
@@ -1,78 +1,43 @@
-//function voirsuite1() {
-  //document.getElementById('voirsuite1').style.display = 'none';
-    //document.getElementById('suite1').style.display = 'block';
-//}
-function voirsuite2() {
-    document.getElementById('voirsuite2').style.display = 'none';
-    document.getElementById('suite2').style.display = 'block'; z
-}
-function voirsuite3() {
-    document.getElementById('voirsuite3').style.display = 'none';
-    document.getElementById('suite3').style.display = 'block';
-}
-function voirsuite4() {
-    document.getElementById('voirsuite4').style.display = 'none';
-    document.getElementById('suite4').style.display = 'block';
-}
-//function replier1() {
-    //document.getElementById('voirsuite1').style.display = 'block';
-    //document.getElementById('suite1').style.display = 'none';
-//}
-function replier2() {
-    document.getElementById('voirsuite2').style.display = 'block';
-    document.getElementById('suite2').style.display = 'none';
-}
-function replier3() {
-    document.getElementById('voirsuite3').style.display = 'block';
-    document.getElementById('suite3').style.display = 'none';
-}
-
-function replier4()
-{
-    document.getElementById('voirsuite4').style.display='block';   
-    document.getElementById('suite4').style.display='none';
-}
-
-
-function darkmode() {
-    document.getElementById('button-on').style.display='none';   
-    document.getElementById('button-off').style.display='block';
-}
-
-function lightmode() {
-    document.getElementById('button-off').style.display='none';   
-    document.getElementById('button-on').style.display='block';
-   
-}
-
+// Select language
 function languageOn() {
-   document.getElementById('lang-selection').style.display='none';
-    document.getElementById('lang-selection').style.display='block';
+    document.getElementById('lang-selection').style.display = 'none';
+    document.getElementById('lang-selection').style.display = 'block';
 }
 
 function replierLanguage() {
-    document.getElementById('lang-selection').style.display='block';
-     document.getElementById('lang-selection').style.display='none';
+    document.getElementById('lang-selection').style.display = 'block';
+    document.getElementById('lang-selection').style.display = 'none';
+}
 
 
- }
+// Dark/Light mode
+function darkmode() {
+    document.getElementById('button-on').style.display = 'none';
+    document.getElementById('button-off').style.display = 'block';
+}
 
- let buttons = document.querySelector('.button');
+function lightmode() {
+    document.getElementById('button-off').style.display = 'none';
+    document.getElementById('button-on').style.display = 'block';
+}
 
- let body = document.querySelector('body');
+let buttons = document.querySelector('.button');
+let body = document.querySelector('body');
 
- body.className = document.cookie;
+body.className = document.cookie;
+
+buttons.addEventListener('click', () => {
+    body.classList.toggle('light-mode');
+    document.cookie = body.className;
+    console.log(document.cookie);
+});
 
 
- buttons.addEventListener('click', ()=> {
-            body.classList.toggle('light-mode');
-            document.cookie = body.className;
-            console.log(document.cookie);
-        });
+// Open/close article
+const block = document.getElementsByClassName('block');
 
-let title1 = document.querySelector('.more1');
-let text1 = document.querySelector('.text1');
-
-title1.addEventListener('click', () => {
-    text1.classList.toggle('visible');
-})
+for (i = 0; i < block.length; i++) {
+    block[i].addEventListener('click', function () {
+        this.classList.toggle('active')
+    })
+}
